bf97b5d4ed1d78b3d851ddcc6a2f15f0522ed1a5
maccy <zmact46@gmail.com>
2025-05-17 16:26:54 -0400

    Player Sprite With Animation and Body Loaded In

--- core/src/main/java/io/github/calvin/LevelScreen.java
+++ core/src/main/java/io/github/calvin/LevelScreen.java
@@ -21,8 +21,20 @@ public class LevelScreen implements Screen , ControllerListener
     World world;
     Box2DDebugRenderer b2ddr;
     OrthographicCamera orthoCamera;
+    float totalElapsedTime = 0.0f;
 
-    //Sample Object
+    //Sprites
+    PlayerSprite player;
+    int playerState = 0;
+
+    //Sample Player World-Object
+    BodyDef playerBodyDef;
+    Body playerBody;
+    PolygonShape playerShape;
+    FixtureDef playerFixtureDef;
+    Fixture playerFixture;
+
+    //Sample World-Object
     BodyDef bodyDef;
     Body body;
     CircleShape circle;
@@ -49,8 +61,10 @@ public class LevelScreen implements Screen , ControllerListener
         firstController = Controllers.getCurrent();
         try{ firstController.addListener(this);}
         catch(NullPointerException npe)
-        {}
+        {
+        }
 
+        generateSprites();
         generateWorld();
     }
 
@@ -65,17 +79,17 @@ public class LevelScreen implements Screen , ControllerListener
         groundBody = world.createBody(groundBodyDef);
 
         groundBox = new PolygonShape();
-        groundBox.setAsBox(orthoCamera.viewportWidth, 10.0f/game.PIXELS_IN_METERS);
+        groundBox.setAsBox(orthoCamera.viewportWidth, 10.0f / game.PIXELS_IN_METERS);
         groundBody.createFixture(groundBox, 0.0f);
 
-        //Sample Object
+        //Sample World-Object
         bodyDef = new BodyDef();
         bodyDef.type = BodyType.DynamicBody;
         bodyDef.position.set(1, 5);
         body = world.createBody(bodyDef);
 
         CircleShape circle = new CircleShape();
-        circle.setRadius(15f/(game.PIXELS_IN_METERS/2));
+        circle.setRadius(15f / (game.PIXELS_IN_METERS / 2));
 
         fixtureDef = new FixtureDef();
         fixtureDef.shape = circle;
@@ -83,15 +97,43 @@ public class LevelScreen implements Screen , ControllerListener
         fixtureDef.friction = 0.4f;
         fixtureDef.restitution = 0.6f;
         fixture = body.createFixture(fixtureDef);
+        circle.dispose();
 
         body.applyForceToCenter(3f, 0.0f, true);
 
+        //Sample Player World-Object
+        playerBodyDef = new BodyDef();
+        playerBodyDef.type = BodyType.DynamicBody;
+        //FIXME This should be optimized since the player size can be initially set to always match the scale
+        playerBodyDef.position.set(new Vector2(player.getX() + (player.getWidth() / 25 / 2), player.getY() + (player.getHeight() / 25 / 2)));
+        playerBody = world.createBody(playerBodyDef);
+        playerBody.setFixedRotation(true);
+
+        playerShape = new PolygonShape();
+        //This just works -- dividing by half the in game PIXELS-to-METERS Ratio
+        playerShape.setAsBox(player.getWidth() / (game.PIXELS_IN_METERS / 2) , player.getHeight() / (game.PIXELS_IN_METERS / 2));
+
+        playerFixtureDef = new FixtureDef();
+        playerFixtureDef.shape = playerShape;
+        playerFixtureDef.density = 0.134f;
+        playerFixtureDef.friction = 0.1f;
+        playerFixtureDef.restitution = 0.0f;
+        playerFixture = playerBody.createFixture(playerFixtureDef);
+        playerShape.dispose();
+
         b2ddr = new Box2DDebugRenderer();
+    }
+    
+    public void generateSprites()
+    {
+        player = new PlayerSprite(5.0f, 2.0f);
     }
 
     @Override
     public void render(float delta)
     {
+        totalElapsedTime += delta;
+
         ScreenUtils.clear(Color.GRAY);
 
         renderWorld(delta);
@@ -102,21 +144,42 @@ public class LevelScreen implements Screen , ControllerListener
 
         game.batch.begin();
 
-        game.font.draw(game.batch, "Gameplay shown here", 100/game.PIXELS_IN_METERS, 150/game.PIXELS_IN_METERS);
+        game.font.draw(game.batch, "Gameplay shown here", 100 / game.PIXELS_IN_METERS, 150 / game.PIXELS_IN_METERS);
+
+        player.draw(game.batch);
 
         game.batch.end();
+
+        updateEntities(totalElapsedTime, delta);
     }
+
+    //FIXME implement keylistener, to listen for keyboard events just like a game controller
+    //Change character state variables in render and use that to update character state
 
     private void renderWorld(float delta)
     {
         world.step(1 / 60f, 6, 2);
+
+        player.setPosition(playerBody.getPosition().x - (player.getWidth() / 25 / 2), playerBody.getPosition().y - (player.getHeight() / 25 / 2));
+
         b2ddr.render(world, orthoCamera.combined); //Matrix4 debug matrix
 
 
-        if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT))
-            body.applyForceToCenter(2f, 0.0f, true);
-        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT))
+        //BALL
+        if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT))
+        {
             body.applyForceToCenter(-2f, 0.0f, true);
+        }
+        else if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT))
+        {
+            body.applyForceToCenter(2f, 0.0f, true);
+        }
+    }
+    
+    private void updateEntities(float totalElapsedTime, float delta)
+    {
+        
+        player.update(totalElapsedTime, delta, 1);
     }
     
 
@@ -128,7 +191,6 @@ public class LevelScreen implements Screen , ControllerListener
     @Override
     public void dispose() {
         groundBox.dispose();
-        circle.dispose();
     }
 
     
@@ -146,12 +208,19 @@ public class LevelScreen implements Screen , ControllerListener
 
     @Override
     public boolean buttonDown(Controller controller, int buttonCode) {
+
         if (firstController.getButton(firstController.getMapping().buttonDpadRight))
-            body.applyForceToCenter(2f, 0.0f, true);
+        {
+            //playerState = 1;
+        }
         else if (firstController.getButton(firstController.getMapping().buttonDpadLeft))
-            body.applyForceToCenter(-2f, 0.0f, true);
+        {
+            //playerState = 2;
+        }
         else if (firstController.getButton(firstController.getMapping().buttonA))
-            body.applyForceToCenter(0.0f, 40.0f, true);
+        {
+            //playerState = 3;
+        }
         //throw new UnsupportedOperationException("Unimplemented method 'buttonDown'");
         return true;
     }
@@ -159,6 +228,8 @@ public class LevelScreen implements Screen , ControllerListener
     @Override
     public boolean buttonUp(Controller controller, int buttonCode) {
         
+        //IF DPAD NOT STILL HELD DOWN
+        //playerState = 0;
         //throw new UnsupportedOperationException("Unimplemented method 'buttonUp'");
         return false;
     }
--- core/src/main/java/io/github/calvin/PlayerSprite.java
+++ core/src/main/java/io/github/calvin/PlayerSprite.java
@@ -11,5 +11,54 @@ import com.badlogic.gdx.utils.Array;
 
 public class PlayerSprite extends Sprite
 {
+    private TextureAtlas atlas;
+    private Array<TextureAtlas.AtlasRegion> walkRight;
+    private Array<TextureAtlas.AtlasRegion> walkLeft;
+    private Animation walkRightAnimation;
+    private Animation walkLeftAnimation;
+
+    private final float ANIMATION_FRAME_SPEED = 0.2f;
+
+    public PlayerSprite(float x, float y)
+    {
+        atlas = new TextureAtlas(Gdx.files.internal("sprites/calvin.atlas"));
+
+        walkRight = atlas.findRegions("walk_right");
+        
+        //Construct a walk left region by just flipping the walk right region
+        walkLeft = atlas.findRegions("walk_right");
+
+        for (TextureAtlas.AtlasRegion tar : walkLeft)
+        {
+            tar.flip(true, false);
+        }
+
+        //Set the boundaries for 'cutting' frames for the animation
+        //System.out.println( "Width: " + walkRight.get(0).getRegionWidth() + " Height: " + walkRight.get(0).getRegionHeight());
+        setBounds(x, y, walkRight.get(0).getRegionWidth(), walkRight.get(0).getRegionHeight());
+        setScale(1/25.0f);
+
+        //Set the initial region of this character
+        setRegion(walkRight.get(0));
+
+        //Construct Animations
+        walkRightAnimation = new Animation<TextureAtlas.AtlasRegion>(ANIMATION_FRAME_SPEED, walkRight);
+        walkLeftAnimation = new Animation<TextureAtlas.AtlasRegion>(ANIMATION_FRAME_SPEED, walkLeft);
+        walkRightAnimation.setPlayMode(Animation.PlayMode.LOOP);
+        walkLeftAnimation.setPlayMode(Animation.PlayMode.LOOP);
+    }
     
+    public void update(float totalElapsedTime, float delta, int movementState)
+    {
+        switch (movementState) {
+            case 1:
+                setRegion((TextureAtlas.AtlasRegion) walkRightAnimation.getKeyFrame(totalElapsedTime));
+                break;
+            case 2:
+                setRegion((TextureAtlas.AtlasRegion) walkLeftAnimation.getKeyFrame(totalElapsedTime));
+            default:
+        }
+    }
+    //1 - Walking Right
+    //2 - Walking Left
 }
